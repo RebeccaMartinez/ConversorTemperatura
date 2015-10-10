bee25bb5f431a380dadf0391c26c74e7f8c4cce1
Rebecca <rebe.mart.gal@gmai.com>
2015-10-10 14:37:52 +0100

    permitido usar numeros flotantes

--- temperature.js
+++ temperature.js
@@ -3,13 +3,41 @@ function calculate() {
   var result1;
   var result2;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])/;
+  var regexp = /(^[-+]?\d+(?:\.\d*)?)(?:[eE]?([-+]?\d+))?\s*([fFcCkK])/;
   var m = temp.match(regexp);
 
   if (m) {
     var num = m[1];
-    var type = m[2];
+    var type = m[3];
     num = parseFloat(num);
+
+    if(m[2]!==undefined){
+        var exp = m[2];
+        exp = parseInt(exp);
+        var a = 1;
+        var b = 10;
+
+        if(exp<0){
+            exp = -exp;
+
+            while(a < exp){
+              b = b * 10;
+              a++;
+            }
+            if(b !== 0){
+              num = num/b;
+            }
+        }
+        else{
+          while(a < exp){
+            b = b * 10;
+            a++;
+          }
+        if(b !== 0)
+          num = num*b;
+        }
+    }
+
     if (type == 'c' || type == 'C') {
       result1 = (num * 9/5)+32;
       result1 = result1.toFixed(1)+" Farenheit"
@@ -19,7 +47,7 @@ function calculate() {
     else if (type == 'k' || type == 'K') {
       result1 = num - 273.15;
       result2 = 1.8 * (num - 273.15) + 32;
-      result1 =   result1 = result1.toFixed(1)+" Celsius"
+      result1 = result1.toFixed(1)+" Celsius"
       result2 = result2.toFixed(1) + " Farenheit"
 
     }
@@ -29,6 +57,7 @@ function calculate() {
       result1 = result1.toFixed(1)+" Celsius"
       result2 = result2.toFixed(1) + " Kelvin"
     }
+
     document.getElementById("converted").setAttribute("col", "gr");
     converted.innerHTML = result1 + ", " +result2;
   }
